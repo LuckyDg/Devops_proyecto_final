ef6fa824725635b64700eefec9d23e5bcbc67cec
LuckyDg <sadgdev@gmail.com>
2024-12-19 18:42:18 -0500

    update files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-  value = aws_instance.web.id
+  value = aws_instance.docker_host.id
 }
 
 output "instance_public_ip" {
-  value = aws_instance.web.public_ip
+  value = aws_instance.docker_host.public_ip
 }
 
 output "public_ip" {
